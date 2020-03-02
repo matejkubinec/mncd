26013090529e9896323a1064d241d8fe09c4727e
Matej Kubinec <32638572+matejkubinec@users.noreply.github.com>
2020-03-02 18:50:05 +0100

    AB#15 EdgeList enhancements
    * Updated edge list to optionally include metadata
    
    * Added reading edgelist with metadata

--- src/MNCD/Readers/EdgeListReader.cs
+++ src/MNCD/Readers/EdgeListReader.cs
@@ -12,56 +12,134 @@ namespace MNCD.Readers
             var layers = new Dictionary<string, Layer>();
             var rows = new List<EdgeListRow>();
 
-            foreach (var line in input.Split('\n'))
+            var lines = input.Split('\n');
+            for (var i = 0; i < lines.Length; i++)
             {
-                var values = line.Split(" ");
-
-                if (values.Length != 5)
+                if (i == lines.Length - 1 && string.IsNullOrWhiteSpace(lines[i]))
                 {
-                    throw new ArgumentException("Invalid edgelist.");
+                    break;
                 }
 
-                var a1 = values[0];
-                var l1 = values[1];
-                var a2 = values[2];
-                var l2 = values[3];
-                var w = values[4];
-
-                if (!double.TryParse(w, out var value))
+                if (lines[i].StartsWith("# Actors"))
                 {
-                    throw new ArgumentException("Invalid weight.");
+                    i++;
+                    for (; i < lines.Length; i++)
+                    {
+                        if (lines[i].StartsWith("# Layers"))
+                        {
+                            break;
+                        }
+
+                        var values = lines[i].Trim().Split(" ");
+
+                        if (i == lines.Length - 1 && string.IsNullOrWhiteSpace(lines[i]))
+                        {
+                            break;
+                        }
+
+                        if (values.Length != 2)
+                        {
+                            throw new ArgumentException("Invalid edgelist actors metadata.");
+                        }
+
+                        var a = values[0];
+                        var n = values[1];
+
+                        if (actors.ContainsKey(a))
+                        {
+                            actors[a].Name = n;
+                        }
+                        else
+                        {
+                            actors.Add(a, new Actor(n));
+                        }
+                    }
+
+                    i++;
+                    for (; i < lines.Length; i++)
+                    {
+                        if (lines[i].StartsWith("# Layers"))
+                        {
+                            break;
+                        }
+
+                        var values = lines[i].Trim().Split(" ");
+
+                        if (i == lines.Length - 1 && string.IsNullOrWhiteSpace(lines[i]))
+                        {
+                            break;
+                        }
+
+                        if (values.Length != 2)
+                        {
+                            throw new ArgumentException("Invalid edgelist layers metadata.");
+                        }
+
+
+                        var l = values[0];
+                        var n = values[1];
+
+                        if (layers.ContainsKey(l))
+                        {
+                            layers[l].Name = n;
+                        }
+                        else
+                        {
+                            layers.Add(l, new Layer() { Name = n });
+                        }
+                    }
                 }
-
-                var row = new EdgeListRow
+                else
                 {
-                    Actor1 = a1,
-                    Layer1 = l1,
-                    Actor2 = a2,
-                    Layer2 = l2,
-                    Weight = w
-                };
+                    var values = lines[i].Trim().Split(" ");
 
-                if (!actors.ContainsKey(a1))
-                {
-                    actors.Add(a1, new Actor { Name = a1 });
+                    if (values.Length != 5)
+                    {
+                        throw new ArgumentException("Invalid edgelist.");
+                    }
+
+                    var a1 = values[0];
+                    var l1 = values[1];
+                    var a2 = values[2];
+                    var l2 = values[3];
+                    var w = values[4];
+
+                    if (!double.TryParse(w, out var value))
+                    {
+                        throw new ArgumentException("Invalid weight.");
+                    }
+
+                    var row = new EdgeListRow
+                    {
+                        Actor1 = a1,
+                        Layer1 = l1,
+                        Actor2 = a2,
+                        Layer2 = l2,
+                        Weight = w
+                    };
+
+                    if (!actors.ContainsKey(a1))
+                    {
+                        actors.Add(a1, new Actor { Name = a1 });
+                    }
+
+                    if (!actors.ContainsKey(a2))
+                    {
+                        actors.Add(a2, new Actor { Name = a2 });
+                    }
+
+                    if (!layers.ContainsKey(l1))
+                    {
+                        layers.Add(l1, new Layer { Name = l1 });
+                    }
+
+                    if (!layers.ContainsKey(l2))
+                    {
+                        layers.Add(l2, new Layer { Name = l2 });
+                    }
+
+                    rows.Add(row);
                 }
-
-                if (!actors.ContainsKey(a2))
-                {
-                    actors.Add(a2, new Actor { Name = a2 });
-                }
-
-                if (!layers.ContainsKey(l1))
-                {
-                    layers.Add(l1, new Layer { Name = l1 });
-                }
-
-                if (!layers.ContainsKey(l2))
-                {
-                    layers.Add(l2, new Layer { Name = l2 });
-                }
-
-                rows.Add(row);
             }
 
             var network = new Network();
@@ -81,10 +159,10 @@ namespace MNCD.Readers
                 var a1 = actors[row.Actor1];
                 var a2 = actors[row.Actor2];
                 var l1 = layers[row.Layer1];
-                var l2 = layers[row.Layer1];
+                var l2 = layers[row.Layer2];
                 var w = double.Parse(row.Weight);
 
-                if (row.Layer1 == row.Layer2)
+                if (l1 == l2)
                 {
                     var edge = new Edge
                     {
@@ -99,7 +177,7 @@ namespace MNCD.Readers
                     var edge = new InterLayerEdge
                     {
                         From = a1,
-                        To = a1,
+                        To = a2,
                         LayerFrom = l1,
                         LayerTo = l2,
                         Weight = w
